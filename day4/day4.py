23e89fcf016645813bd3daf20275788ac1848518
LWLeijten <larsleijten@gmail.com>
2020-12-04 17:24:48 +0100

    Day4 solution
    Solution to day4

---
+++ day4/day4.py
@@ -0,0 +1,81 @@
+MANDATORY = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+OPTIONAL = ['cid']
+EYE_COLOURS = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+
+
+def in_range(x, min, max):
+    return int(x) in range(min, max + 1)
+
+
+def birth_validity(x): return in_range(x, 1920, 2002)
+def issue_validity(x): return in_range(x, 2010, 2020)
+def expiration_validity(x): return in_range(x, 2020, 2030)
+def eye_validity(x): return x in EYE_COLOURS
+def pid_validity(x): return len(x) == 9 and all(map(lambda d: d.isdigit(), x))
+def country_validity(x): return True
+
+
+def hair_validity(x): return x[0] == '#' and len(x) == 7 and all(
+    map(lambda c: c.isdigit() or (c >= 'a' and c <= 'f'), x[1:]))
+
+
+def height_validity(x):
+    postfix = x[-2:]
+    numbers = x[:-2]
+    if postfix == 'cm':
+        return in_range(numbers, 150, 193)
+    elif postfix == 'in':
+        return in_range(numbers, 59, 76)
+    return False
+
+
+validator_mapping = {
+    'byr': birth_validity,
+    'iyr': issue_validity,
+    'eyr': expiration_validity,
+    'hgt': height_validity,
+    'hcl': hair_validity,
+    'ecl': eye_validity,
+    'pid': pid_validity,
+    'cid': country_validity
+}
+
+
+def read_input():
+    """Reads the input of the problem and stores it as a list of dicts"""
+    passports = []
+    with open('day4/input.txt') as f:
+        cur_passport = {}
+        for line in f:
+            if not line.strip():
+                passports.append(cur_passport)
+                cur_passport = {}
+                continue
+            else:
+                for kv in line.split():
+                    k = kv.split(':')[0]
+                    v = kv.split(':')[1]
+                    cur_passport[k] = v
+        passports.append(cur_passport)
+    return passports
+
+
+def check_field_presences(passport):
+    """ Checks if all mandatory fields are present in the passport. Oneliner is more for fun than for readability."""
+    return set(passport.keys()) in (set(MANDATORY + OPTIONAL), set(MANDATORY))
+
+
+def check_validity(passport):
+    """ Checks if all the needed key value pairs in the passport are present 
+        and pass their respective validator tests.
+        Oneliner is more for fun than for readability. """
+    return check_field_presences(passport) and all(map(lambda kv: validator_mapping[kv[0]](kv[1]), passport.items()))
+
+
+# Find the solutions for part 1 and 2 of the puzzle.
+if __name__ == "__main__":
+    passports = read_input()
+    # Part 1 - check if all fields are present
+    print(len(list(filter(check_field_presences, passports))))
+    # Part 2 - check if all fields are present and valid
+    print(len(list(filter(check_validity, passports))))
