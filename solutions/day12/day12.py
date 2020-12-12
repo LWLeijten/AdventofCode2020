98e1ce3e11c2f4de7cf2e4d1e0d868d662e5f9e4
LWLeijten <larsleijten@gmail.com>
2020-12-12 16:57:33 +0100

    Made day 12 a bit more modular.

--- solutions/day12/day12.py
+++ solutions/day12/day12.py
@@ -24,6 +24,14 @@ cardinal_to_degree = {
     'W': 270
 }
 
+""" Movement deltas for each of the cardinal directions. """
+directions = {
+    'N': (0, 1),
+    'E': (1, 0),
+    'S': (0, -1),
+    'W': (-1, 0)
+}
+
 
 def read_input():
     """Reads the input of the problem and returns it as a list of instructions"""
@@ -35,68 +43,81 @@ def read_input():
     return instructions
 
 
-def rotate_clockwise(waypoint):
+def rotate_clockwise(waypointx, waypointy):
     """ Rotates the waypoint 90 degrees clockwise. """
-    n = waypoint[0]
-    e = waypoint[1]
-    return [-e, n]
+    return waypointy, -waypointx
 
 
-def rotate_counterclockwise(waypoint):
+def rotate_counterclockwise(waypointx, waypointy):
     """ Rotates the waypoint 90 degrees counter-clockwise. """
-    n = waypoint[0]
-    e = waypoint[1]
-    return [e, -n]
+    return -waypointy, waypointx
+
+
+def rotate_waypoint(wptx, wpty, direction, degrees):
+    """ Rotates the waypoints current positions into the given direction
+        by the given amount of degrees. """
+    times = int(degrees / 90)
+    if direction == 'L':
+        rotate_function = rotate_counterclockwise
+    elif direction == 'R':
+        rotate_function = rotate_clockwise
+    for _ in range(times):
+        wptx, wpty = rotate_function(wptx, wpty)
+    return wptx, wpty
+
+
+def move_obj(x, y, direction, amount):
+    """ Moves an object from current x and y
+        to a new x y given by the direction and amount of movement.
+        Returns the new position of the object."""
+    x += directions[direction][0] * amount
+    y += directions[direction][1] * amount
+    return x, y
+
+
+def rotate_boat(facing, direction, degrees):
+    """ Rotates the boat given its current facing, the direction of rotation
+        and the degrees of rotation. Returns the new facing."""
+    if direction == 'R':
+        cur_degrees = cardinal_to_degree[facing] + degrees
+    elif direction == 'L':
+        cur_degrees = cardinal_to_degree[facing] - degrees
+    return degree_to_cardinal[cur_degrees % 360]
 
 
 def part1(instructions):
     """ Performs all instructions according to the rules of part 1.
         Returns the manhattan distance from start to end."""
-    direction = 'E'
-    steps = {'N': 0, 'E': 0, 'S': 0, 'W': 0}
+    facing = 'E'
+    x_pos = y_pos = 0
     for i in instructions:
         action = i.action
         if action in ['N', 'E', 'S', 'W']:
-            steps[action] += i.value
+            x_pos, y_pos = move_obj(x_pos, y_pos, action, i.value)
         elif action == 'F':
-            steps[direction] += i.value
-        elif action == 'R':
-            cur_degrees = cardinal_to_degree[direction] + i.value
-            direction = degree_to_cardinal[cur_degrees % 360]
-        elif action == 'L':
-            cur_degrees = cardinal_to_degree[direction] - i.value
-            direction = degree_to_cardinal[cur_degrees % 360]
-    return abs(steps['N'] - steps['S']) + abs(steps['E'] - steps['W'])
+            x_pos, y_pos = move_obj(x_pos, y_pos, facing, i.value)
+        elif action in ['R', 'L']:
+            facing = rotate_boat(facing, action, i.value)
+    return abs(x_pos) + abs(y_pos)
 
 
 def part2(instructions):
     """ Performs all instructions according to the rules of part 2.
         Not elegant, but it works.
         Returns the manhattan distance from start to end."""
-    waypoint = [1, 10]
-    deltas = [0, 0]
+    wptx = 10
+    wpty = 1
+    x_pos = y_pos = 0
     for i in instructions:
         action = i.action
-        if action == 'N':
-            waypoint[0] += i.value
-        elif action == 'E':
-            waypoint[1] += i.value
-        elif action == 'S':
-            waypoint[0] -= i.value
-        elif action == 'W':
-            waypoint[1] -= i.value
+        if action in ['N', 'E', 'S', 'W']:
+            wptx, wpty = move_obj(wptx, wpty, action, i.value)
         elif action == 'F':
-            deltas[0] += waypoint[0] * i.value
-            deltas[1] += waypoint[1] * i.value
-        elif action == 'R':
-            times = int(i.value / 90)
-            for _ in range(times):
-                waypoint = rotate_clockwise(waypoint)
-        elif action == 'L':
-            times = int(i.value / 90)
-            for _ in range(times):
-                waypoint = rotate_counterclockwise(waypoint)
-    return abs(deltas[0]) + abs(deltas[1])
+            x_pos += wptx * i.value
+            y_pos += wpty * i.value
+        elif action in ['R', 'L']:
+            wptx, wpty = rotate_waypoint(wptx, wpty, action, i.value)
+    return abs(x_pos) + abs(y_pos)
 
 
 # Find the solutions for part 1 and 2 of the puzzle.
