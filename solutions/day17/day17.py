6025038b96efb10c555187aa1b7acc389ea20240
LWLeijten <larsleijten@gmail.com>
2020-12-24 16:52:44 +0100

    Great improvement for day 17
    Refactored day 17 based on knowledge gained from day 24. A lot shorter and cleaner code PLUS a large performance increase. Win-win.

--- solutions/day17/day17.py
+++ solutions/day17/day17.py
@@ -1,124 +1,71 @@
-from collections import namedtuple
-from copy import copy
-
-""" ROUGH CODE BELOW:
-    THIS DAY IS THE NUMBER 1 IN LIST OF NEEDED REFACTORS.
-    VERY INEFFICIENT AND MANY BAD CODE SMELLS.
-    ALREADY HAVE AN IDEA USING A DEFAULT-DICT AND MORE EFFICIENT
-    TRAVERSING THROUGH THE CUBE, BUT HAVENT HAD MUCH TIME YET
-    WHILST KEEPING UP WITH THE NEW PUZZLES AS WELL. """
-
-
-Cube = namedtuple('Cube', 'x y z active')
-Cube4d = namedtuple('Cube4d', 'x y z w active')
+from collections import defaultdict
+from itertools import product
+import operator
 
 
 def read_input():
-    cubes3d = []
-    cubes4d = []
+    """ Reads the input file and returns it as a default dict 
+        containing active cubes, for both 3d and 4d space. """
+    cubes3d = defaultdict(lambda: 0)
+    cubes4d = defaultdict(lambda: 0)
     z = w = 0
     with open('solutions/day17/input.txt') as f:
         for y, line in enumerate(f):
             for x, char in enumerate(line.strip()):
-                cubes3d.append(Cube(x, y, z, char == '#'))
-                cubes4d.append(Cube4d(x, y, z, w, char == '#'))
+                if char == '#':
+                    cubes3d[(x, y, z)] = 1
+                    cubes4d[(x, y, z, w)] = 1
     return cubes3d, cubes4d
 
 
-def get_neighbours3d(cube, cubes):
-    return list(filter(lambda c: c.x in [cube.x - 1, cube.x, cube.x + 1] and
-                       c.y in [cube.y - 1, cube.y, cube.y + 1] and
-                       c.z in [cube.z - 1, cube.z, cube.z + 1] and
-                       not c == cube, cubes))
+def get_neighbours(cube):
+    """ Gets all the neighbours of a cube, agnostic to the amount of dimensions. 
+        Remove the cube itself as a neighbour as final step."""
+    neighbours = []
+    for direction in product([-1, 0, 1], repeat=len(cube)):
+        neighbours.append(tuple(map(operator.add, cube, direction)))
+    neighbours.remove(cube)
+    return neighbours
 
 
-def get_bounds3d(cubes):
-    min_x = min(map(lambda c: c.x, cubes))
-    max_x = max(map(lambda c: c.x, cubes))
-    min_y = min(map(lambda c: c.y, cubes))
-    max_y = max(map(lambda c: c.y, cubes))
-    min_z = min(map(lambda c: c.z, cubes))
-    max_z = max(map(lambda c: c.z, cubes))
-    return (min_x, max_x), (min_y, max_y), (min_z, max_z)
+def get_relevant_cubes(cube_dict):
+    """ Creates a set of cubes that we have to check the
+        game of life rules against for a given round. """
+    actives = list(cube_dict.keys())
+    neighbours = set()
+    for a in actives:
+        nbs = get_neighbours(a)
+        for nb in nbs:
+            neighbours.add(nb)
+    targets = actives + list(neighbours)
+    return targets
 
 
-def expand_cubes3d(xs, ys, zs, cubes):
-    for x in range(xs[0] - 1, xs[1] + 2):
-        for y in range(ys[0] - 1, ys[1] + 2):
-            for z in range(zs[0] - 1, zs[1] + 2):
-                if len(list(filter(lambda c: c.x == x and c.y == y and c.z == z, cubes))) == 0:
-                    cubes.append(Cube(x, y, z, False))
-    return cubes
+def simulate_round(cube_dict):
+    """ Simulates a round of the game of life.
+        First find the target cubes, secondly check them against the decision rules.
+        Returns a dict of only active cubes after the round is over. """
+    targets = get_relevant_cubes(cube_dict)
+    output_dict = defaultdict(lambda: 0)
+    for t in targets:
+        is_active = cube_dict[t]
+        a_neighbours = len(
+            list(filter(lambda nb: cube_dict[nb] == 1, get_neighbours(t))))
+        if is_active and a_neighbours in [2, 3]:
+            output_dict[t] = 1
+        elif not is_active and a_neighbours == 3:
+            output_dict[t] = 1
+    return output_dict
 
 
-def cycle3d(cubes):
-    xs, ys, zs = get_bounds3d(cubes)
-    cubes = expand_cubes3d(xs, ys, zs, cubes)
-    new_cubes = []
-    for cube in cubes:
-        active_neighbours = len(
-            list(filter(lambda c: c.active, get_neighbours3d(cube, cubes))))
-        if cube.active and active_neighbours in [2, 3]:
-            new_cubes.append(cube)
-        elif not cube.active and active_neighbours == 3:
-            new_cubes.append(Cube(cube.x, cube.y, cube.z, True))
-        else:
-            new_cubes.append(Cube(cube.x, cube.y, cube.z, False))
-    return list(filter(lambda c: c.active, new_cubes))
-
-
-def get_neighbours4d(cube, cubes):
-    return list(filter(lambda c: c.x in [cube.x - 1, cube.x, cube.x + 1] and
-                       c.y in [cube.y - 1, cube.y, cube.y + 1] and
-                       c.z in [cube.z - 1, cube.z, cube.z + 1] and
-                       c.w in [cube.w - 1, cube.w, cube.w + 1] and
-                       not c == cube, cubes))
-
-
-def get_bounds4d(cubes):
-    min_x = min(map(lambda c: c.x, cubes))
-    max_x = max(map(lambda c: c.x, cubes))
-    min_y = min(map(lambda c: c.y, cubes))
-    max_y = max(map(lambda c: c.y, cubes))
-    min_z = min(map(lambda c: c.z, cubes))
-    max_z = max(map(lambda c: c.z, cubes))
-    min_w = min(map(lambda c: c.w, cubes))
-    max_w = max(map(lambda c: c.w, cubes))
-    return (min_x, max_x), (min_y, max_y), (min_z, max_z), (min_w, max_w)
-
-
-def expand_cubes4d(xs, ys, zs, ws, cubes):
-    for x in range(xs[0] - 1, xs[1] + 2):
-        for y in range(ys[0] - 1, ys[1] + 2):
-            for z in range(zs[0] - 1, zs[1] + 2):
-                for w in range(ws[0] - 1, ws[1] + 2):
-                    if len(list(filter(lambda c: c.x == x and c.y == y and c.z == z and c.w == w, cubes))) == 0:
-                        cubes.append(Cube4d(x, y, z, w, False))
-    return cubes
-
-
-def cycle4d(cubes):
-    xs, ys, zs, ws = get_bounds4d(cubes)
-    cubes = expand_cubes4d(xs, ys, zs, ws, cubes)
-    new_cubes = []
-    for cube in cubes:
-        active_neighbours = len(
-            list(filter(lambda c: c.active, get_neighbours4d(cube, cubes))))
-        if cube.active and active_neighbours in [2, 3]:
-            new_cubes.append(cube)
-        elif not cube.active and active_neighbours == 3:
-            new_cubes.append(Cube4d(cube.x, cube.y, cube.z, cube.w, True))
-        else:
-            new_cubes.append(Cube4d(cube.x, cube.y, cube.z, cube.w, False))
-    return list(filter(lambda c: c.active, new_cubes))
-
-
-cubes3d, cubes4d = read_input()
-for i in range(6):
-    cubes3d = cycle3d(cubes3d)
-    print(f"Completed cycle {i}")
-print(len(list(filter(lambda c: c.active, cubes3d))))
-for i in range(6):
-    cubes4d = cycle4d(cubes4d)
-    print(f"Completed cycle {i}")
-print(len(list(filter(lambda c: c.active, cubes4d))))
+# Find the solutions for part 1 and 2 of the puzzle.
+if __name__ == "__main__":
+    cubes3d, cubes4d = read_input()
+    for i in range(6):
+        cubes3d = simulate_round(cubes3d)
+        print(f"Completed cycle {i}")
+    print(len(cubes3d))
+    for i in range(6):
+        cubes4d = simulate_round(cubes4d)
+        print(f"Completed cycle {i}")
+    print(len(cubes4d))
